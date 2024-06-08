0b001a95297a2aa9ab4a8d3fd34b15eced8fc882
Mohaiminul375 <mohaiminul375@gmail.com>
2024-06-08 13:32:26 +0600

    review delete by admin or moderator

--- src/component/ReviewCard.jsx
+++ src/component/ReviewCard.jsx
@@ -1,19 +1,80 @@
+import { FaTrash } from "react-icons/fa6";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 const ReviewCard = ({ review }) => {
-  console.log(review);
-  const { university_name, subject, applicant_name,post_date,review_point,review_comment } = review;
+  const axiosSecure = useAxiosSecure();
+  const queryClient = useQueryClient();
+  const {
+    _id,
+    university_name,
+    subject,
+    applicant_name,
+    post_date,
+    review_point,
+    review_comment,
+  } = review;
+  const { mutateAsync } = useMutation({
+    mutationFn: async (id) => {
+      const { data } = await axiosSecure.delete(`/all-reviews/${id}`);
+      return data;
+    },
+    onSuccess: (data) => {
+      if (data.deletedCount > 0) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+        queryClient.invalidateQueries({ queryKey: ["my-reviews"] });
+        queryClient.invalidateQueries({ queryKey: ["all-reviews"] });
+      }
+    },
+  });
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        mutateAsync(id);
+      }
+    });
+  };
+
   return (
     <div className="card w-80 bg-[#EEFAFC] shadow-xl">
       <div className="flex flex-col p-4">
         <div>
-          <h2 className="text-xl font-bold text-[#1E62D5]">University Name: {university_name}</h2>
+          <h2 className="text-xl font-bold text-[#1E62D5]">
+            University Name: {university_name}
+          </h2>
           <p className="text-sm font-bold text-[#1E62D5]">Subject: {subject}</p>
           <hr className="border-[#247CFF] my-3" />
         </div>
         <div>
-          <h2 className="text-xl font-bold text-[#1E62D5]">Reviewer Name: {applicant_name}</h2>
-          <p className="text-sm font-bold text-[#1E62D5]">{new Date(post_date).toLocaleDateString()}</p>
-          <p className="text-base text-[#1E62D5]">Rating Point: {review_point}</p>
+          <h2 className="text-xl font-bold text-[#1E62D5]">
+            Reviewer Name: {applicant_name}
+          </h2>
+          <p className="text-sm font-bold text-[#1E62D5]">
+            {new Date(post_date).toLocaleDateString()}
+          </p>
+          <p className="text-base text-[#1E62D5]">
+            Rating Point: {review_point}
+          </p>
           <p className="text-base text-[#1E62D5]">{review_comment}</p>
+        </div>
+        <div className="flex justify-end">
+          <FaTrash
+            onClick={() => handleDelete(_id)}
+            className="text-2xl text-white bg-red-600 p-1 rounded-md cursor-pointer"
+          />
         </div>
       </div>
     </div>
